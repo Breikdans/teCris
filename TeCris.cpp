ec6931bfaaf4ba1c0efede94e41583786b09afd1
Breikdans <Breikdans@users.noreply.github.com>
2015-02-26 15:35:51 +0100

    Update TeCris.cpp

--- TeCris.cpp
+++ TeCris.cpp
@@ -10,21 +10,36 @@ int main(int argc, char* argv[])
 {
 	// INICIALIZACION
 	Juego TeCris;
-
+	unsigned long mTimeIni = 0;
+	unsigned long mTimeNow = 0;
+	
 	TeCris.Init();
+	
+	// Tiempo Actual(SDL)
+	mTimeIni = SDL_GetTicks();	
 
 	// BUCLE DEL JUEGO
 	do{
-		// DIBUJAR / RENDERIZAR
-		TeCris.Draw();
-
-		// GESTION EVENTO DE ENTRADA
+		// EVENTOS - GESTION EVENTO DE ENTRADA
 		TeCris.Input();
 
-		// ACTUALIZAR ESTADOS
-		TeCris.Update();
+		// LOGICA - ACTUALIZAR ESTADOS
+		// TeCris.Update();
+		
+		// Si se ha cumplido el tiempo de bajar la pieza
+		mTimeNow = SDL_GetTicks();
+		if(mTimeNow - mTimeIni > mGameSpeed)
+		{
+			// LOGICA - ACTUALIZAR ESTADOS
+			TeCris.Update();
+			
+			mTimeIni = SDL_GetTicks();
+		}
+		
+		// RENDERIZAR - DIBUJAR
+		TeCris.Draw();
 
-		SDL_Delay(50);
+//		SDL_Delay(50);
 	} while (TeCris.IsRunning());
 
 	atexit(SDL_Quit);
